9867399db59a219e7e38824b22006a3136360925
Lencerd <i2006lol@gmail.com>
2025-06-06 00:46:20 +0500

    Delete Scripts directory

--- Scripts/Другое/Адаптация.js
+++
@@ -1,9 +0,0 @@
-function adjustMainPadding() {
-    const header = document.querySelector('header');
-    const main = document.querySelector('main');
-    const headerHeight = header.offsetHeight;
-    main.style.paddingTop = `${headerHeight + 20}px`;
-  }
-
-  window.addEventListener('load', adjustMainPadding);
-  window.addEventListener('resize', adjustMainPadding);
